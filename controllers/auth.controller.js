53b1baaea4da92019fe727cab1f0c029b734f9a4
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2024-11-27 19:08:24 +0100

    feat: login page with basic controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,34 @@
+const User = require('../models/User.model')
+
+module.exports.login = (req, res, next) => {
+  res.render('auth/login')
+}
+
+module.exports.doLogin = (req, res, next) => {
+  // Funcion para renderizar el formulario con el error
+  const renderWithErrors = () => {
+    res.render('auth/login', { error: 'Email o contraseña incorrectos', email: req.body.email })
+  }
+
+  // Busco si hay un usuario con ese email
+  // User.findOne({$or: [ { email: req.body.email }, { username: req.body.email } ]})
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return renderWithErrors()
+      }
+
+      // Aqui asumo que si tengo usuario, compruebo las contraseñas
+      return user.checkPassword(req.body.password)
+        .then(match => {
+          if (!match) {
+            return renderWithErrors()
+          }
+
+          res.redirect('/')
+        })
+    })
+    .catch(err => {
+      next(err)
+    })
+}
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ const upload = require('../config/multer.config')
 // Controllers
 const miscController = require('../controllers/misc.controller')
 const usersController = require('../controllers/users.controller')
+const authController = require('../controllers/auth.controller')
 
 
 // Misc urls
@@ -16,6 +17,8 @@ router.get('/register', usersController.create)
 router.post('/register', upload.single('image'), usersController.doCreate)
 
 // User login
+router.get('/login', authController.login)
+router.post('/login', authController.doLogin)
 
 // Products
 
